f933e734f275dfdb8be6cb108ee419a373ecf289
Alex Horton <alexhortonmusic@gmail.com>
2016-09-26 10:56:49 -0500

    GET title from Node/added bower components

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// do not use res.render or templates(pug) with a MEAN app
+// use res.send OR res.json instead
+
 const express = require('express')
 
 const app = express()
@@ -9,5 +12,9 @@ app.set('port', port)
 
 // middlewares
 app.use(express.static('client')) // base directory is root, not where server is
+
+app.get('/api/title', (req, res) =>
+  res.send({ title: 'MEAN 101 from Node' })
+)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
